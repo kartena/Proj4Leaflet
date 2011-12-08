6ca3d2cca218fd51491b90b012609e25d3967132
Christopher Freden <christopher.freden@kartena.se>
2011-12-08 11:59:53 +0100

    Changed tile map server url and removed max bounds

--- examples/script.js
+++ examples/script.js
@@ -1,6 +1,5 @@
 
 var res = [8192, 4096, 2048, 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1, 0.5]
-,bounds = new L.LatLngBounds(new L.LatLng(55.2, 10.57), new L.LatLng(69.1,24.18))
 ,start = new L.LatLng(57.704503026010514, 11.965263344824994)
 ,map = new L.Map('map', {
   crs: proj4leaflet.crs('EPSG:2400'
@@ -8,10 +7,9 @@ var res = [8192, 4096, 2048, 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1, 0.5]
   ,scale: function(zoom) {
     return 1 / res[zoom];
   }
-  ,maxBounds: bounds
 })
 // Change the mapUrl below to test!
-,mapUrl = 'http://api.geosition.com/tilestache/gp/{z}/{x}/{y}.png'
+,mapUrl = 'http://api.geosition.com/tile/lmv/{z}/{x}/{y}.png'
 ,attrib = 'Map data &copy; 2011 Lantmäteriet, Imagery &copy; 2011 Kartena'
 ,tilelayer = new L.TileLayer(mapUrl, {
   scheme: 'xyz'
