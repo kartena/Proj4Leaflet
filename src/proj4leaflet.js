bac1d1c3561d915930e6bb3769e187bfd59961b2
Aarni Koskela <akx@iki.fi>
2017-08-14 14:18:39 +0300

    Use .default exports for Proj4 if they exist.

--- src/proj4leaflet.js
+++ src/proj4leaflet.js
@@ -15,7 +15,13 @@
 		factory(window.L, window.proj4);
 	}
 }(function (L, proj4) {
-
+	if (proj4.__esModule && proj4.default) {
+		// If proj4 was bundled as an ES6 module, unwrap it to get
+		// to the actual main proj4 object.
+		// See discussion in https://github.com/kartena/Proj4Leaflet/pull/147
+		proj4 = proj4.default;
+	}
+ 
 	L.Proj = {};
 
 	L.Proj._isProj4Obj = function(a) {
